4c859cbd75dcc327675c20d2dc9db4443fc0f994
Anderson Perrelli <anderson.perrelli@liferay.com>
2022-03-16 22:37:02 -0300

    App Engine Provision

--- providers.tf
+++ providers.tf
@@ -14,3 +14,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-a"
 }
+
+provider "google-beta" {
+  project = "mba-lab-devops-aperrelli-6aso"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
